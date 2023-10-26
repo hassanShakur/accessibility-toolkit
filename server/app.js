fafddc3bc04b5265698b3ddc652b91bd729b5b85
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-26 09:33:04 +0300

    final server middleware setup

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}))
+
+app.use(bodyParser.json())
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONT_END_URL,
+}))
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 module.exports = app;
 

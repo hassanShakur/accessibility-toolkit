1a7fbc28cf0e369ac83ce79299e0798e178d8a0a
hassanShakur <hass.shakurhassan@gmail.com>
2023-11-10 23:12:44 +0300

    initial side bar design

--- client/app/report/layout.tsx
+++ client/app/report/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import './report.scss';
 
 export const metadata: Metadata = {
   title: 'Accessibility Toolkit | Site Report',
@@ -10,20 +11,18 @@ const ReportLayout = ({
 }: {
   children: React.ReactNode;
 }) => {
-  // A side nav that can be toggled on smaller screens
+  // A side nav that can be toggled using a hamburger menu icon on smaller screens
   return (
     <div className='report-layout'>
-      <div className='report-layout__side-nav'>
-        <div className='report-layout__side-nav__logo'>
-          <h1>AxeTool</h1>
-          <p>The accessibility toolkit handler</p>
-        </div>
-        <div className='report-layout__side-nav__links'>
+      <aside>
+        <h1 id='logo'>AxeTool</h1>
+        <div className='aside-links'>
           <a href='/'>Home</a>
           <a href='/report'>Report</a>
         </div>
-      </div>
-      <div className='report-layout__content'>{children}</div>
+        <div id="side-design"></div>
+      </aside>
+      <div className='report-content'>{children}</div>
     </div>
   );
 };

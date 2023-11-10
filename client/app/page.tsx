18f7b52ccb8f16a70629aacbb12e5e44071ec224
hassanShakur <hass.shakurhassan@gmail.com>
2023-11-10 14:50:28 +0300

    Update client and server code and styling

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,29 +1,39 @@
-'use client'
+'use client';
 import { useState, FormEvent } from 'react';
 import axios from 'axios';
+import './page.scss';
 
-const backendUrl = 'http://localhost:7000/api/scrape'
+const backendUrl = 'http://localhost:7000/api/scrape';
 
 const Home = () => {
-    const [url, setUrl] = useState('')
+  const [url, setUrl] = useState('');
 
-    const urlSubmitHandler = async (e: FormEvent) => {
-        e.preventDefault();
-        try {
-            const res = await axios.post(backendUrl, {url})
-            console.log(res.data)
-        } catch (error) {
-            console.log(error)
-        }
+  const urlSubmitHandler = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(backendUrl, { url });
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
   return (
-    <form onSubmit={urlSubmitHandler}>
-        <input type="text" onChange={(e)=>setUrl(e.target.value)} placeholder='Enter site url...' />
+    <main>
+      <div className='hero'>
+        <h1>AxeTool</h1>
+        <p>The accessibility toolkit handler</p>
+      </div>
+      <form onSubmit={urlSubmitHandler}>
+        <input
+          type='text'
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder='Enter site url...'
+        />
         <button type='submit'>Submit</button>
-    </form>
-  )
-}
+      </form>
+    </main>
+  );
+};
 
 export default Home;
-

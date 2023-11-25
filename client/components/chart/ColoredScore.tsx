ebf7b57ec07494a4506cb7b4edcc41edc2e31582
hassanShakur <hass.shakurhassan@gmail.com>
2023-11-25 14:50:46 +0300

    add nav glassy back

--- client/components/chart/ColoredScore.tsx
+++ client/components/chart/ColoredScore.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
+export const calcScoreColor = (score: number) => {
+  if (score >= 90) {
+    return '#00ff00';
+  } else if (score >= 50) {
+    return '#ffa500';
+  } else {
+    return '#ff0000';
+  }
+};
 
 const ColoredScore = ({ score }: { score: number }) => {
-  const calcScoreColor = (score: number) => {
-    if (score >= 90) {
-      return '#00ff00';
-    } else if (score >= 50) {
-      return '#ffa500';
-    } else {
-      return '#ff0000';
-    }
-  };
-
   return (
     <h3
       style={{
--- client/components/chart/GeneralChart/AccessDough.tsx
+++ client/components/chart/GeneralChart/AccessDough.tsx
@@ -5,6 +5,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import { calcScoreColor } from '../ColoredScore';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -22,7 +23,10 @@ const AccessDough = (props: propsType) => {
       {
         label: 'Total',
         data: [totalScore - accessibilityScore, accessibilityScore],
-        backgroundColor: ['#004948', '#0be982'],
+        backgroundColor: [
+          '#464a5050',
+          calcScoreColor(accessibilityScore),
+        ],
         borderWidth: 0,
       },
     ],
@@ -36,10 +40,48 @@ const AccessDough = (props: propsType) => {
     },
   };
 
+  const textCenterPlugin = {
+    id: 'textCenter',
+    beforeDraw: (chart: any) => {
+      const { ctx, data } = chart;
+      const { width, height } = chart;
+      const formattedAccessibilityScore = Math.ceil(
+        accessibilityScore
+      );
+
+
+      ctx.save();
+      ctx.font = 'bold 1.5rem sans-serif';
+      ctx.fillStyle = calcScoreColor(accessibilityScore);
+      ctx.textAlign = 'center';
+      ctx.textBaseline = 'middle';
+      ctx.fillText(
+        `${formattedAccessibilityScore}%`,
+        width / 2,
+        height / 2
+      );
+      // const width = chart.chart.width,
+      //   height = chart.chart.height,
+
+      // ctx.restore();
+
+      // const text = accessibilityScore + '%',
+      //   textX = Math.round((width - ctx.measureText(text).width) / 2),
+      //   textY = height / 2;
+
+      // ctx.fillText(text, textX, textY);
+      // ctx.save();
+    },
+  };
+
   return (
     <div id='dough'>
       <h1>Accessibility</h1>
-      <Doughnut data={data} options={options}></Doughnut>
+      <Doughnut
+        data={data}
+        options={options}
+        plugins={[textCenterPlugin]}
+      ></Doughnut>
     </div>
   );
 };

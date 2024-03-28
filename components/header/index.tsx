6349d867aee3ab50c61202b9dc82fd8d7eac90ee
Hassan Shakur <dev.hassanshakur@gmail.com>
2024-03-28 22:06:57 +0300

    supabase initial setup

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,17 +1,51 @@
+'use client';
+
+import { createClient } from '@supabase/supabase-js';
 import SearchBar from './SearchBar';
 import './header.scss';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const supabaseUrl = 'https://petdmhhtxrcenqsdbyhl.supabase.co';
+  const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBldGRtaGh0eHJjZW5xc2RieWhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzNjA3NTQsImV4cCI6MjAxOTkzNjc1NH0.ZKU-orJJsiKm37eKGYrItSNjNUh7NLmS2GpjOEuPHBU';
+  const supabase = createClient(supabaseUrl, supabaseKey);
+  const [userData, setUserData] = useState<any>(null);
+
+  const signIn = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    supabase.auth.getUser().then((user) => {
+      setUserData(user);
+    });
+  }, [supabase.auth]);
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    console.log({ error });
+  };
+
   return (
     <header>
       <SearchBar />
       <nav>
         <ul>
           <li>
-            <a href='#'>Sign In</a>
+            <button onClick={signIn}>Sign In</button>
           </li>
           <li>
-            <a href='#'>Sign Up</a>
+            <button>Sign Up</button>
           </li>
         </ul>
       </nav>

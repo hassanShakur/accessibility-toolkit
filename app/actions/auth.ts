19d00dd7ba4cee5c37039e87c9f7c5f75762d6c1
Hassan Shakur <dev.hassanshakur@gmail.com>
2024-04-04 15:26:54 +0300

    show logout btn conditionally

--- app/actions/auth.ts
+++ app/actions/auth.ts
@@ -7,6 +7,18 @@ const supabaseKey =
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
+
 export const signIn = async () => {
   console.log({ supabaseKey });
   await supabase.auth.signInWithOAuth({
@@ -16,6 +28,7 @@ export const signIn = async () => {
         access_type: 'offline',
         prompt: 'consent',
       },
+      redirectTo: getURL(),
     },
   });
 };
--- components/sideNav/index.tsx
+++ components/sideNav/index.tsx
@@ -1,11 +1,15 @@
 import Link from 'next/link';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { userSignOut } from '@/redux/authSlice';
 import './sideNav.scss';
+import { RootState } from '@/redux/store';
 
 const SideNav = () => {
   const dispatch = useDispatch();
+  const currUser = useSelector<RootState, any>(
+    (state) => state.auth.user
+  );
 
   return (
     <aside>
@@ -17,14 +21,16 @@ const SideNav = () => {
       </div>
 
       <div id='logout'>
-        <button
-          onClick={() => {
-            dispatch(userSignOut() as any);
-          }}
-          className='bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-md w-full'
-        >
-          Logout
-        </button>
+        {currUser && (
+          <button
+            onClick={() => {
+              dispatch(userSignOut() as any);
+            }}
+            className='bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded-md w-full'
+          >
+            Logout
+          </button>
+        )}
       </div>
     </aside>
   );
